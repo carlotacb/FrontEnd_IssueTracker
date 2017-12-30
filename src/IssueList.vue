ccc025f7205b3be78901073fb839f28aca09499f
Aleix Balletbó <aleix.balletbo@est.fib.upc.edu>
2017-12-31 00:46:12 +0100

    canvis a l'estil de la taula d'issues

--- src/IssueList.vue
+++ src/IssueList.vue
@@ -2,42 +2,66 @@
   <div id="issueList">
     <h1>Issues</h1>
     <div id="filters">
-      <p class="filter-label">Filters:</p>
-      <ul class="filter-status">
+      <ul class="filter-status"> Filters:
         <button v-on:click="all">All</button>
         <button v-on:click="open">Open</button>
         <button v-on:click="assignee(current_user.id)">My Issues</button>
         <button v-on:click="watching(current_user.id)">Watching</button>
       </ul>
     </div>
-    <h2>Issues ({{issues.length}})</h2>
-    <table class="issue-list">
-    <thead>
-      <tr>
-          <th scope="col">Title</th>
-          <th scope="col">Type</th>
-          <th scope="col">Priority</th>
-          <th scope="col">Status</th>
-          <th scope="col">Votes</th>
-          <th scope="col">Assignee</th>
-          <th scope="col">Created</th>
-          <th scope="col">Updated</th>
-          <th colspan="3"></th>
-        </tr>
-    </thead>
-    <tbody>
-      <tr v-for="issue in issues">
-        <td v-if="issue.id" class="td-clickable"><router-link :to="{ name: 'issue', params: { id: issue.id }}">{{ issue.Title }}</router-link></td>
-        <td class="td-clickable" v-on:click="type(issue.Type)">{{ issue.Type }}</td>
-        <td class="td-clickable" v-on:click="priority(issue.Priority)">{{ issue.Priority }}</td>
-        <td class="td-clickable" v-on:click="status(issue.Status)">{{ issue.Status }}</td>
-        <td>{{ issue.Votes }}</td>
-        <td v-if="issue._links" class="td-clickable" v-on:click="assignee(issue._links.assignee.id)">{{ issue._links.assignee.name }}</td>
-        <td>{{ issue.created_at | humanReadableTime }}</td>
-        <td>{{ issue.updated_at | humanReadableTime }}</td>
-      </tr>  
-    </tbody>
+    <h3>Issues ({{issues.length}})</h3>
+    <b-table striped hover responsive="md" :items="issues" :fields="fields">
+      <template slot="Title" scope="data">
+        <router-link :to="{ name: 'issue', params: { id: data.item.id }}">{{ data.item.Title }}</router-link>
+      </template>
+      <template slot="Type" scope="data">
+        <span class="td-clickable" v-on:click="type(data.item.Type)">{{ data.item.Type }}</span>
+      </template>
+      <template slot="Priority" scope="data">
+        <span class="td-clickable" v-on:click="priority(data.item.Priority)">{{ data.item.Priority }}</span>
+      </template>
+      <template slot="Status" scope="data">
+        <span class="td-clickable" v-on:click="status(data.item.Status)">{{ data.item.Status }}</span>
+      </template>
+      <template slot="_links" scope="data">
+        <span class="td-clickable" v-on:click="assignee(data.value.assignee.id)">{{ data.value.assignee.name }}</span>
+      </template>
+      <template slot="created_at" scope="data">
+        {{data.item.created_at | humanReadableTime}}
+      </template>
+      <template slot="updated_at" scope="data">
+        {{data.item.updated_at | humanReadableTime}}
+      </template>
+    </b-table>
+    <!--
+    <table>
+      <thead>
+        <tr>
+            <th scope="col">Title</th>
+            <th scope="col">Type</th>
+            <th scope="col">Priority</th>
+            <th scope="col">Status</th>
+            <th scope="col">Votes</th>
+            <th scope="col">Assignee</th>
+            <th scope="col">Created</th>
+            <th scope="col">Updated</th>
+            <th colspan="3"></th>
+          </tr>
+      </thead>
+      <tbody>
+        <tr v-for="issue in issues">
+          <td v-if="issue.id" class="td-clickable"><router-link :to="{ name: 'issue', params: { id: issue.id }}">{{ issue.Title }}</router-link></td>
+          <td class="td-clickable" v-on:click="type(issue.Type)">{{ issue.Type }}</td>
+          <td class="td-clickable" v-on:click="priority(issue.Priority)">{{ issue.Priority }}</td>
+          <td class="td-clickable" v-on:click="status(issue.Status)">{{ issue.Status }}</td>
+          <td>{{ issue.Votes }}</td>
+          <td v-if="issue._links" class="td-clickable" v-on:click="assignee(issue._links.assignee.id)">{{ issue._links.assignee.name }}</td>
+          <td>{{ issue.created_at | humanReadableTime }}</td>
+          <td>{{ issue.updated_at | humanReadableTime }}</td>
+        </tr>  
+      </tbody>
     </table>
+    -->
     <ul v-if="errors && errors.length">
       <li v-for="error of errors">
         {{error.message}}
@@ -49,6 +73,9 @@
 <script>
 import moment from 'moment'
 import {HTTP} from './http-common';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 export default {
   name: 'issueList',
   filters: {
@@ -59,6 +86,7 @@ export default {
   data () {
     return {
       currentUser: {},
+      fields: ['Title', 'Type', 'Priority', 'Status', 'Votes', {key:'_links', label:'Assignee'}, {key:'created_at', label:'Created'}, {key:'updated_at', label:'Updated'}],
       issues: [],
       errors: []
     }
@@ -129,17 +157,20 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style>
 #issueList {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
+  margin-left: 30px;
+  margin-right: 30px;
+  margin-bottom: 30px;
 }
 
 .td-clickable:hover {
-  color: #42b983
+  color: #008ae6
 }
 
 h1, h2 {
@@ -149,10 +180,5 @@ h1, h2 {
 ul {
   list-style-type: none;
   padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
 }
 </style>
